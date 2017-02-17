f4191b97cdb56eeebe3cd01b1ac0d9806bfc1126
rajbharath <rbrajbharath1@gmail.com>
2017-02-17 18:04:50 +0530

    Added Hoare partitioning method & perfomance comparision between hoare & lomuto paritioning methods

--- quick-sort.js
+++ quick-sort.js
@@ -1,17 +1,28 @@
 var stackDepth = 0;
 
-function quicksort(data, low, high, _depth) {
+function quicksort_hoare(data, low, high, _depth) {
   stackDepth = stackDepth + _depth;
-  var partitioned_index = _place_pivot_in_its_place(data, low, high);
+  var partitioned_index = _hoare_partition(data, low, high);
   if (low < partitioned_index) {
-    quicksort(data, low, partitioned_index, 1);
+    quicksort_hoare(data, low, partitioned_index, 1);
   }
   if (partitioned_index + 1 < high) {
-    quicksort(data, partitioned_index + 1, high, 0);
+    quicksort_hoare(data, partitioned_index + 1, high, 0);
   }
 }
 
-function _place_pivot_in_its_place(data, low, high) {
+function quicksort_lomuto(data, low, high, _depth) {
+  stackDepth = stackDepth + _depth;
+  var partitioned_index = _lomuto_partition(data, low, high);
+  if (low < partitioned_index) {
+    quicksort_lomuto(data, low, partitioned_index, 1);
+  }
+  if (partitioned_index + 1 < high) {
+    quicksort_lomuto(data, partitioned_index + 1, high, 0);
+  }
+}
+
+function _lomuto_partition(data, low, high) {
   var left = low + 1;
   var pivot = data[low];
   for( var i = left; i < high; i++ ) {
@@ -25,14 +36,40 @@ function _place_pivot_in_its_place(data, low, high) {
   return pivot_index;
 }
 
+function _hoare_partition(data, low, high) {
+  var pivot = data[low];
+  var left = low;
+  var right = high;
+  while(true) {
+    left++;
+    right--;
+
+    for(;data[left] < pivot; ++left);
+    for(;data[right] > pivot; --right);
+
+    if ( left >= right ) {
+      _swap(data, low, right);
+      return right;
+    }
+    _swap(data, left, right);
+  }
+}
+
 function _swap(data, i, j) {
   var temp = data[i];
   data[i] = data[j];
   data[j] = temp;
 }
 
-function doSort(data) {
-  quicksort(data, 0, data.length, 1);
+function doSort(data, method) {
+  switch(method) {
+    case 'hoare':
+      quicksort_hoare(data, 0, data.length, 1);
+      break;
+    default:
+      quicksort_lomuto(data, 0, data.length, 1);
+
+  }
 }
 
 function getShuffledData(n) {
@@ -46,8 +83,6 @@ function getShuffledData(n) {
   return data;
 }
 
-var data = getShuffledData(104000000);
-
 function isDataSortedTest(data) {
   for (var i = 0; i < data.length - 1; i++) {
     if ( data[i] > data[i + 1]) {
@@ -58,14 +93,20 @@ function isDataSortedTest(data) {
   }
   return true;
 }
-console.log('-------------------');
-console.log('Start Time');
-console.log('-------------------');
-console.log(new Date());
-doSort(data);
-console.log('-------------------');
-console.log(new Date());
-console.log('End Time');
-console.log('-------------------');
-console.log(isDataSortedTest(data));
-console.log(stackDepth);
+
+function performanceTest(data, method) {
+  stackDepth = 0;
+  console.log('-----PARTITION METHOD----------');
+  console.log(method);
+  console.log('-------Start Time-----');
+  console.log(new Date());
+  doSort(data, method);
+  console.log('--------End Time-----------');
+  console.log(new Date());
+  console.log(isDataSortedTest(data));
+  console.log(stackDepth);
+}
+
+var data = getShuffledData(100000000);
+performanceTest(data.slice(0), 'lomuto');
+performanceTest(data.slice(0), 'hoare');
